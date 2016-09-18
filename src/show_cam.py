7bf7435f27d828ab53c1315948a511b5588d4879
Joe Reinhart <jreinhart@desktop.jamborsky>
2016-09-18 19:02:33 -0400

    Attempt some filtering in show_cam.py

--- src/show_cam.py
+++ src/show_cam.py
@@ -25,6 +25,21 @@ ratio = 1.0
 screenCnt = None
 
 
+def show_blue(f):
+    #f = cv2.inRange(f, (100, 0, 255), (255, 255, 255))
+
+    cv2.cvtColor(f, cv2.COLOR_BGR2HSV, dst=f)
+    #f = cv2.inRange(f, (100, 0, 100), (255, 255, 255))
+
+    #cv2.cvtColor(f, cv2.COLOR_BGR2HLS, dst=f)
+    #f = cv2.inRange(f, (0, 100, 100), (255, 255, 255))
+
+
+
+    return f
+
+def angle2hex(a):
+    return (a/360.0) * 255
 
 while(True):
     step_count += 1
@@ -32,6 +47,8 @@ while(True):
     # Capture frame-by-frame
     ret, frame = feed.read()
     if not ret: continue
+
+    frame = show_blue(frame)
 
     cv2.imshow('Camera View', frame)
 
