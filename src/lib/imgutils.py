280fc582228f22dbfe6b8e0244d20a059da04e76
Joe Reinhart <jreinhart@desktop.jamborsky>
2016-10-02 13:24:45 -0400

    Cleaned up pylint issues in imgutils

--- src/lib/imgutils.py
+++ src/lib/imgutils.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 
+'''
+Filename:   imgutils.py
+Date:       02 Oct 2016
+Version:    1.0
+Author:     jayemar
+
+Library of functions for common image manipulation tasks
+'''
+
 import cv2
-import pdb
 import numpy as np
 
-color_dict = {'red': [0, 1], 'green': [0, 2], 'blue': [1, 2]}
+COLOR_DICT = {'red': [0, 1], 'green': [0, 2], 'blue': [1, 2]}
 
 def scale_2d(matrix, height=None, width=None):
+    '''
+    Scale 2-dimensional matrix to height and/or width, if specified.
+
+    If either height or width is specified, the output matrix will have that
+    height or width with the other dimension being scaled so as to keep the
+    height/width ratio the same as the input matrix.
+
+    If neither height nor width are specified, the output matrix will have the
+    same dimensions as the input matrix.
+
+    Parameters:
+        height  - optional; height of output matrix
+        width   - optional; width of output matrix
+    Return value:
+        2-dimensional matrix with specified height and width
+    '''
     if len(matrix.shape) != 2:
         raise ValueError("The Matrix must have 2 and only 2 dimensions")
 
     # These dimensions will be required for scaling
-    orig_m = matrix.copy()
-    orig_dim = matrix.shape
     orig_h = matrix.shape[0]
     orig_w = matrix.shape[1]
-    orig_ratio = float(orig_w) / float(orig_h)
 
     dim = (0, 0)
 
@@ -37,24 +58,61 @@ def scale_2d(matrix, height=None, width=None):
     else:
         dim = (height, width)
 
-    dim = tuple(map(lambda x: int(x), dim))
+    dim = tuple([int(x) for x in dim])
     return cv2.resize(matrix, dim, interpolation=cv2.INTER_AREA)
 
 def get_color_mask(matrix, color_hex, tolerance=0.10):
-    # Color hex is in 'RGB' form
+    '''
+    Get color mask for a specified RGB hex color value
+
+    Parameters:
+        color_hex   - hex value in RGB form
+        tolerance   - optional; how close searched values will match color_hex
+    Return value:
+        grayscale mask matrix
+    '''
     (min_val, max_val) = __get_color_min_max(color_hex, tolerance)
     mask = cv2.inRange(matrix, min_val, max_val)
     return mask
 
 
+def match_template(template_path, img_path,
+                   img_xy_start=(0.0, 0.0), img_xy_end=(1.0, 1.0)):
+    '''
+    Return a location and value for the largest match between an image and a
+    template
+
+    Parameters:
+        template_path   - file path for the template image
+        img_pag         - file path for the main image
+        img_xy_start    - tuple of starting (minX, minY) decimal to begin match
+        img_xy_end      - tuple of ending (minX, minY) decimal to end match
+    Return value:
+        tuple of the form (minVal, maxVal, (minLoc), (maxLoc))
+    '''
+    template = cv2.imread(template_path)
+    img = cv2.imread(img_path)
+    match = cv2.matchTemplate(img, template, cv2.TM_CCORR)
+    return cv2.minMaxLoc(match)
+
+
 def show_primary(matrix, color, single_channel=False):
+    '''
+    Show only the single layer of the image representing the specified primary
+    color
+
+    Parameters:
+        color   - one of 'red', 'green', or 'blue'
+    Return value:
+        1-dimensional matrix
+    '''
     if __color_check(color):
         channels = __get_channels(matrix)
-        for c in color_dict[color]:
-            channels[c] = np.zeros(channels[c].shape, dtype="uint8")
+        for color in COLOR_DICT[color]:
+            channels[color] = np.zeros(channels[color].shape, dtype="uint8")
 
         resp = np.array([])
-        if (single_channel):
+        if single_channel:
             resp = channels[__get_color_index(color)]
         else:
             resp = cv2.merge(channels)
@@ -62,6 +120,15 @@ def show_primary(matrix, color, single_channel=False):
 
 
 def remove_primary(matrix, color="blue"):
+    '''
+    Show the image representing without the layer representing the specified
+    primary color
+
+    Parameters:
+        color   - one of 'red', 'green', or 'blue'
+    Return value:
+        3-dimensional matrix
+    '''
     if __color_check(color):
         channels = __get_channels(matrix)
         p_index = __get_color_index(color)
@@ -70,32 +137,31 @@ def remove_primary(matrix, color="blue"):
 
 
 def __get_color_min_max(color_hex, tolerance=0.10):
-    if (len(color_hex) != 6):
+    if len(color_hex) != 6:
         raise ValueError("The color must be a 6-digit hex value in RGB form")
 
     t_diff = int(tolerance * 255)
 
-    # TODO: Find a more pythonic way to write this
-    min_R = int(color_hex[0:2], 16) - t_diff
-    min_G = int(color_hex[2:4], 16) - t_diff
-    min_B = int(color_hex[4:6], 16) - t_diff
-    max_R = int(color_hex[0:2], 16) + t_diff
-    max_G = int(color_hex[2:4], 16) + t_diff
-    max_B = int(color_hex[4:6], 16) + t_diff
+    min_red = int(color_hex[0:2], 16) - t_diff
+    max_red = int(color_hex[0:2], 16) + t_diff
+    min_green = int(color_hex[2:4], 16) - t_diff
+    max_green = int(color_hex[2:4], 16) + t_diff
+    min_blue = int(color_hex[4:6], 16) - t_diff
+    max_blue = int(color_hex[4:6], 16) + t_diff
 
-    return ((min_B, min_G, min_R), (max_B, max_G, max_R))
+    return ((min_blue, min_green, min_red), (max_blue, max_green, max_red))
 
 
 def __get_channels(matrix, color=None):
-    if color is not None and color not in color_dict.keys():
-        raise ValueError("Color must be one of %s" % color_dict.keys())
+    if color is not None and color not in COLOR_DICT.keys():
+        raise ValueError("Color must be one of %s" % COLOR_DICT.keys())
     return cv2.split(matrix)
 
 def __color_check(color=None):
-    if color is not None and color not in color_dict.keys():
-        raise ValueError("Color must be one of %s" % color_dict.keys())
+    if color is not None and color not in COLOR_DICT.keys():
+        raise ValueError("Color must be one of %s" % COLOR_DICT.keys())
     else:
         return True
 
 def __get_color_index(color):
-    return list(set([0,1,2]) - set(color_dict[color])).pop()
+    return list(set([0, 1, 2]) - set(COLOR_DICT[color])).pop()
